9ab8727c90f22dca484f0d68606288c3193d1c1e
jlynam <jack.lynam.19@cnu.edu>
2021-02-16 18:57:42 -0500

    Adding solution to github

---
+++ includes/fileio.h
@@ -0,0 +1,20 @@
+#ifndef FILEIO_H_
+#define FILEIO_H_
+
+#include <string>
+#include <vector>
+#include "constants.h"
+
+using namespace std;
+using namespace myconsts;
+
+namespace JL {
+	//attempt to open file 'filename' and read in all data
+	//returns SUCCESS if all goes well or COULD_NOT_OPEN_FILE
+	int loadData(const std::string filename, vector<process> &myProcesses);
+
+	//attempt to create or open file 'filename' to write all data to
+	//returns SUCCESS if all goes well or COULD_NOT_OPEN_FILE
+	int saveData(const std::string filename, vector<process> &myProcesses);
+}
+#endif
